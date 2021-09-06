33f96d1e4d0acb63abe3682fa4525218d34599bb
jun.otsuka <badmintonjun0528@icloud.com>
2021-09-06 10:26:29 +0000

    user pages

---
+++ app/Http/Controllers/usersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class usersController extends Controller
+{
+    public function index()
+    {
+     // ユーザ一覧をidの降順で取得
+        $users = User::orderBy('id', 'desc')->paginate(10);
+
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+}
+
